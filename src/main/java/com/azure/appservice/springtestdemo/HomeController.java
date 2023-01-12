1f5d1fea8551d607692a512e31c2d16cf7c305a3
Thurain <43527185+thurain-oo@users.noreply.github.com>
2023-01-12 13:28:19 +0630

    Update HomeController.java

--- src/main/java/com/azure/appservice/springtestdemo/HomeController.java
+++ src/main/java/com/azure/appservice/springtestdemo/HomeController.java
@@ -12,6 +12,6 @@ public class HomeController {
     public String home() {
         // Use the Guava library as an example of an external dependency.
         Joiner joiner = Joiner.on(" ").skipNulls(); 
-        return joiner.join("<a href=\"https://apithurain.azurewebsites.net\" >http://abcd.efg.com</a>", null, "from", "Spring", "Boot");
+        return joiner.join("<a href='https://apithurain.azurewebsites.net' >http://abcd.efg.com</a>", null, "from", "Spring", "Boot");
     }
 }
